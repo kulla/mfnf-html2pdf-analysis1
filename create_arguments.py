b8cf2da5ca778f09788dbaa99480bc2b6afd7d05
Stephan Kulla <git.mail@kulla.me>
2017-06-30 21:25:55 +0200

    Revise "create_arguments.py"

--- create_arguments.py
+++ create_arguments.py
@@ -1,34 +1,28 @@
 """Script which creates a Makefile for making a PDF of the book „Analysis 1“ of
 the project „Mathe für Nicht-Freaks“."""
 
-import sys
-
+from itertools import chain
 from urllib.parse import quote
 from sitemap import get_sitemap
 
-PAGENO = 0
+def iter_nodes(sitemap_node):
+    """Iterate over all nodes of `sitemap_node` using depth first."""
+    yield sitemap_node
 
-def create_arguments(node, output):
-    """Prints the arguments for creating PDFs of the sitemap node `node` with
-    the tool `wkhtmltopdf` to `output`. `page_nr` is the number of the current
-    page."""
-    global PAGENO
-
-    if node["link"]:
-        title = quote(node["link"])
-        url = "http://de.wikibooks.org/w/index.php?printable=yes&" + \
-              "title=" + title
-        output.write(url + " out/page%04d.pdf\n" % PAGENO)
-        PAGENO += 1
-
-    for child in node["children"]:
-        create_arguments(child, output)
+    yield from chain(*map(iter_nodes, sitemap_node["children"]))
 
 def run_script():
     """Runs this script."""
     book = get_sitemap()["children"][2]
 
-    create_arguments(book, sys.stdout)
+    nodes = [x for x in iter_nodes(book) if x["link"]]
+    links = ["http://de.wikibooks.org/w/index.php?printable=yes&" + \
+             "title=" + quote(x["link"])
+             for x in nodes]
+    targets = ["out/page%04d.pdf" % x for x in range(len(links))]
+
+    for link, target in zip(links, targets):
+        print(link + " " + target)
 
 if __name__ == "__main__":
     run_script()
